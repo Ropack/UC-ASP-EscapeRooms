a71f463ce3b8e2a86ebb4b24cebef754ed2e57bb
Radek Rous <radek.rous@riganti.cz>
2019-04-03 14:20:38 +0200

    Create from template, install castle windsor nupkg

---
+++ US.ASP.EscapeRooms/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace US.ASP.EscapeRooms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
